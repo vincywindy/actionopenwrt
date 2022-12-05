689fcaf1c375b2d9a1b2ccf9fc5819d164f17b01
vincywindy <vincywindy@live.cn>
2022-12-06 03:14:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '1d' feeds.conf.default
+
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git packages https://github.com/vincywindy/packages' feeds.conf.default
 
 
 
