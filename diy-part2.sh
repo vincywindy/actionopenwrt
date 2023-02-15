ceb2dc07434e38238cb038cc457cd6d6b7b20f99
vincywindy <vincywindy@live.cn>
2023-02-15 20:36:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,6 @@
 sed -i 's/bootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+ls
 mv ../config-5.15 openwrt/target/linux/x86/config-5.15
 cat openwrt/target/linux/x86/config-5.15
